9d79df5fa76186458f18831b82a5a356e88c1628
lokeshthotakuri <lokeshthotakuri@users.noreply.github.com>
2024-09-14 19:15:45 +0530

    adding result for missed json

--- main.js
+++ main.js
@@ -32,6 +32,23 @@ const nestedObj3 = {
 		"friends":["bob"]
 	},
 }
+//result: {
+//   'person1.id': '1',
+//   'person1.education.graduation.type': 'be',
+//   'person1.education.graduation.college': 'ii',
+//   'person1.address.city': 'ched',
+//   'person1.address.pincode': '78905',
+//   'person1.friends.0': 'satoshi',
+//   'person1.friends.1': 'vin',
+//   'person2.id': '2',
+//   'person2.education.graduation.type': 'barch',
+//   'person2.education.graduation.college': 'si',
+//   'person2.education.postgraduation.type': 'ms',
+//   'person2.education.postgraduation.college': 'il',
+//   'person2.address.city': 'bin',
+//   'person2.address.pincode': '78965',
+//   'person2.friends.0': 'bob'
+// }
 
 const nestedObj2 = {
 	"pedro": {
@@ -69,3 +86,4 @@ console.log(resultFromObject2);
 
 let resultFromObject3 = objectFlatten(nestedObj3);
 console.log(resultFromObject3);
+
